96cf07bfc804165c57830b6ebd485b2dda1b7977
Palash Thakur <virtuosophantom@gmail.com>
2022-11-10 01:53:33 +0530

    ok

---
+++ JS/q14.js
@@ -0,0 +1,33 @@
+function bat(man, maxPower) {
+
+    power = maxPower;
+    
+    try {
+    
+    man();
+    
+    } finally {
+    
+    let power = 400;
+    
+    } console.log(power);
+}
+function man() {
+
+    try {
+        throw new Error();
+
+function man() 
+{
+
+try {
+
+power = 100;
+
+} catch(e){ power = 200;
+
+} finally { power = 300;
+
+}
+}
+bat(man, 100);
